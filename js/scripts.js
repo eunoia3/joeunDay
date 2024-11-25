a345955f0d29307bfe875844fa00a72c58a55699
Jiyeon Eun <67309720+eunoia3@users.noreply.github.com>
2024-11-25 11:36:29 +0900

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -369,7 +369,7 @@ function shareKakao() {
         });
 */
     Kakao.Share.sendCustom({
-        templateId: '114610',
+        templateId: 114610,
           templateArgs: {
             title: '제목 영역입니다.',
             description: '설명 영역입니다.',
